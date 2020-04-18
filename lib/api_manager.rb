e4e35f2b02de9730865d5b12fc71b85c4169620a
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-04-18 13:11:42 -0400

    Will allow the pages to move after selecting top stories and can move to the previous pages

--- lib/api_manager.rb
+++ lib/api_manager.rb
@@ -1,12 +1,17 @@
 class NewsApp::APIManager
       #Stores API endpoint URL in a constant at the top of the class
 
+      # https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=9a4690dd6f4541548698fc4eff7209ab&pageSize=5
+
       BASE_URL = 'http://newsapi.org/v2/top-headlines?'
 
+      # page = pagenum
+      # pageSize = limit
+
       ARRAY = []
-      def self.getnews #uses the NET::HTTP library to send an HTTP request from our program
-          # url = BASE_URL + API_KEY
-          url = BASE_URL + "country=us&" + API_KEY
+      def self.getnews(page=1, pageSize=5) #uses the NET::HTTP library to send an HTTP request from our program
+          # url = BASE_URL + "country=us&" + API_KEY
+          url = BASE_URL + "country=us&" + "#{(page -1)*pageSize}&pageSize=#{pageSize}&" + API_KEY
 
           uri = URI.parse(url)
           response = Net::HTTP.get_response(uri) #NET::HTTP is a Ruby library that allows your program or application to send HTTP requests.
@@ -24,6 +29,10 @@ class NewsApp::APIManager
             ARRAY << new_hash
             end
             NewsApp::News.mass_create_from_api(ARRAY)
+            return {
+           next: res["next"],
+           prev: res["previous"]
+       }
         end
 
 
@@ -37,13 +46,25 @@ class NewsApp::APIManager
 
         def self.seach_by_query(user_query_search_input)
         #https://newsapi.org/v2/top-headlines?q=corona&apiKey=9a4690dd6f4541548698fc4eff7209ab
+        array_by_search = []
         url = BASE_URL + "q=#{user_query_search_input}&" + API_KEY
         uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
         res = JSON.parse(response.body)
         posts = res["articles"]
         puts posts
+
+        posts.each do |post|
+          new_hash = {
+            title: post["title"],
+            url: post["url"],
+            author: post["author"],
+            description: post["description"],
+            content: post["content"]
+          }
+          array_by_search << new_hash
+          NewsApp::News.populate_data
+          end
+          NewsApp::News.mass_create_from_api(array_by_search)
         end
-
-
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,17 +2,22 @@
 
 class NewsApp::CLI
 
+
+  def initialize
+       @page = 1
+       @pageSize = 5
+   end
+
   def start
     introduction
     get_news_data
-    # news_options_loop
     main_menu
   end
 
   def introduction
     puts "\n\n\n\n\n\n"
     puts "The TOP US News Stories"
-    # sleep(2)
+    sleep(2)
     puts "\n"
   end
 
@@ -26,19 +31,17 @@ class NewsApp::CLI
   end
 
   def menu_input(user_input)
-
     if user_input == "1"
       news_options_loop
     elsif user_input == "2"
       puts "This is option 2"
       query_selection
     elsif user_input == "exit" #MUST GET TO WORK
-      puts "exit"
+      puts "Thank you come again!"
       return
       else
       puts "Invalid Response, Please Try Again"
       main_menu
-       # break if user_input.downcase == "exit"
     end
   end
 
@@ -51,23 +54,42 @@ class NewsApp::CLI
   end
 
   def get_news_data
-      NewsApp::APIManager.getnews
+      NewsApp::APIManager.getnews(@page, @pageSize)
   end
 
   def news_options_loop
     loop do
-      second_menu
-      input = get_article_choice
-      break if input == "exit"
-      next if input == "invalid"
-      return main_menu if input == "return" # if return make input == user_input
-      display_article(input)
-  end
-end
+            second_menu
+            input = get_article_choice
+            case input
+            when "exit"
+                break
+            when "invalid"
+                next
+            when "return" # if return make input == user_input
+                main_menu
+                return
+            when "next"
+                @page += 1
+                _, stop = get_page_range
+                if NewsApp::News.all.length < stop
+                    get_news_data
+                end
+            when "prev"
+                if @page <= 1
+                    puts "You cannot get that page, you are alredy on page 1!"
+                else
+                    @page -= 1
+                end
+            else
+                display_article(input)
+            end
+        end
+      end
 
     def display_article(i)
       a = NewsApp::News.all[i]
-      b = i.to_i - 1
+      b = i- 1
       NewsApp::APIManager.get_more_news_info(i, a) if !a.more? #located in NewsApp.rb
       puts a.full_details
       puts 'Press any key to go back'
@@ -76,14 +98,14 @@ end
 
     def get_article_choice ## MUST ADD RETURN TO MAIN MENU
       input = gets.strip.downcase
-      return main_menu if input == "return"
-      return input if input == "exit"
-      # return input.downcase if commands.include?(input.downcase)
+      commands = ["exit","return","next", "prev"]
+      return input.downcase if commands.include?(input.downcase)
       if !valid?(input)
-        puts "umm... that doesn't make sense"
+        puts "Invalid Response, Please Try Again"
+        sleep(3)
         return "invalid"
       end
-      return input.to_i - 1
+        return input.to_i - 1
     end
 
     def valid?(i)
@@ -99,15 +121,19 @@ end
       puts "Please type what you would like to search"
       user_query_search_input = gets.chomp.downcase
       NewsApp::APIManager.seach_by_query(user_query_search_input)
-
     end
 
     def display_articles
-      # start, stop = get_page_range
-      #  puts "\n\nPAGE #{@page}"
-      # NewsApp::News.all[start...stop].each.with_index do |p,i|
-        NewsApp::News.all.each.with_index do |p,i|
-      puts "#{i+1}. #{p}"
+      start, stop = get_page_range
+        puts "\n\nPAGE #{@page}"
+        NewsApp::News.all[start...stop].each.with_index do |p,i|
+        # NewsApp::News.all.each.with_index do |p,i|
+        puts "#{i+1}. #{p}"
+      end
     end
-end
+
+    def get_page_range
+      [(@page - 1) * @pageSize, @page * @pageSize]
+    end
+
 end
